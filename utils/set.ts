ab0d2afe50f539515a0ab954867ab46760fc8b66
alkhipce <alkhipce@mail.ru>
2021-07-14 23:25:16 +0600

    create set util

---
+++ utils/set.ts
@@ -0,0 +1,40 @@
+type Indexed<T = unknown> = {
+  [key in string]: T;
+};
+
+function set(
+  object: Indexed | unknown,
+  path: string,
+  value: unknown
+): Indexed | unknown | any {
+  if (path.constructor !== String) {
+    throw new Error("path must be string");
+  }
+
+  if ((object as object).constructor !== Object) {
+    return object;
+  }
+
+  let splitPath = path.split(".");
+
+  for (let i = 0; i <= splitPath.length - 1; i++) {
+    const path = splitPath.length ? splitPath.slice(i + 1).join(".") : null;
+    if (path) {
+      const test = {};
+      (object as object)[splitPath[i]] = set(test, path, value);
+    } else {
+      (object as object)[splitPath[i]] = value;
+    }
+
+    return object;
+  }
+}
+
+export default set;
+
+console.log(set({ foo: 5 }, "bar.baz", 10));
+
+/**
+ * set({ foo: 5 }, 'bar.baz', 10); // { foo: 5, bar: { baz: 10 } }
+ * set(3, 'foo.bar', 'baz'); // 3
+ */
